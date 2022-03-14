11b93d1ff173774f1d3563524b2d8fe195f4380e
TheDFD <99446104+TheDFD@users.noreply.github.com>
2022-03-13 23:55:40 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var promptPasswordLength = window.confirm("Please select password criteria");
 
-  passwordText.value = password;
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABSDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 127;
+    var password = " ";
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
